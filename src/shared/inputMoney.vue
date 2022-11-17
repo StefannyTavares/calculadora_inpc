d614c8bbd2c0d56230a260c040820a7c84e96eac
Stefanny Tavares <stefanny.tavares@hotmail.com>
2022-11-17 10:21:19 -0300

    Feat: Added component inputMoney

---
+++ src/shared/inputMoney.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <vuetify-money
+            v-model="model"
+            :label="label"
+            :placeholder="placeholder"
+            :outlined="outlined"
+            :options="options"
+            :dense="dense"
+            persistent-placeholder
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InputMoney',
+    props: {
+        label: {
+            type: String,
+        },
+        placeholder: {
+            type: String,
+        },
+        outlined: Boolean,
+        clearable: Boolean,
+        dense: Boolean,
+        persistentPlaceholder: Boolean,
+    },
+    data() {
+        return{
+            model: '',
+            options: {
+                locale: "pt-BR",
+                prefix: "R$",
+                suffix: "",
+                length: 11,
+                precision: 2,
+            },
+        }
+    },
+
+    watch: {
+        model (novoValor) {
+            this.$emit('input', novoValor);
+        },
+    }
+};
+</script>
+
+<style scoped>
+</style>
